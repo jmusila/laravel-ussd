9505f836bb1f5ce5e8f05cfccdfa1977b94666ce
jmusila <jonathanmusila6@gmail.com>
2021-09-27 09:51:45 +0300

    started working on the ussd

--- app/Http/Controllers/UssdController.php
+++ app/Http/Controllers/UssdController.php
@@ -6,5 +6,18 @@ use Illuminate\Http\Request;
 
 class UssdController extends Controller
 {
-    //
+    /**
+     * Ussd menu
+     */
+    public function ussdMenu(Request $request)
+    {
+        $sessionId   = $request->get('session_id');
+        $serviceCode = $request->get('service_code');
+        $phoneNumber = $request->get('phone_number');
+        $text        = $request->get('text');
+
+        $ussd_string_exploded = explode("*", $text);
+
+        $level = count($ussd_string_exploded);
+    }
 }
